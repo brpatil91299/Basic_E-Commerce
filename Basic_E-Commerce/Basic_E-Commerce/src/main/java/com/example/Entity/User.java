480fed16844ab0bc9d624a031bdbc95718111eac
Bhushan Patil <brpatil91299@gmail.com>
2023-07-09 22:22:26 +0530

    First commit

---
+++ Basic_E-Commerce/Basic_E-Commerce/src/main/java/com/example/Entity/User.java
@@ -0,0 +1,30 @@
+package com.example.Entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userId;
+    private String name;
+    @Email
+    private String email;
+
+
+    private String password;
+
+    @Pattern(regexp = "^\\+\\d{2}\\d{10}$" , message = "Phone no should have 12 digits and first two digits will be county only.")
+    private String phoneNumber;
+
+
+}
---
+++ Basic_E-Commerce/Basic_E-Commerce/src/main/java/com/example/Repository/IUserRepo.java
@@ -0,0 +1,10 @@
+package com.example.Repository;
+
+import com.example.Entity.Address;
+import com.example.Entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IUserRepo extends CrudRepository<User, Integer> {
+}
